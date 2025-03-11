3dd461e189c7b7a35f641b69987b69b6c71d9164
manuely guedes <manuelyufpa@gmail.com>
2025-03-11 19:30:28 -0300

    Iniciando cadastro de filmes.

--- api/src/database/index.js
+++ api/src/database/index.js
@@ -1,6 +1,8 @@
 import { Sequelize, DataTypes } from 'sequelize'
 import Users from '../models/Users.js';
 import Profiles from '../models/Profiles.js';
+import Collection from '../models/Collection.js';
+import Types from '../models/Types.js';
 import registerAssociations from './associations.js';
 
 import databaseConfig from '../config/database.js'
@@ -9,10 +11,14 @@ const connection = new Sequelize(databaseConfig)
 
 const usersModel = Users(connection, DataTypes);
 const profilesModel = Profiles(connection, DataTypes);
+const collectionModel = Collection(connection, DataTypes);
+const typesModel = Types(connection, DataTypes);
 
 const models = {
     Users: usersModel,
     Profiles: profilesModel,
+    Collection: collectionModel,
+    Types: typesModel
 };
 
 registerAssociations(models);
